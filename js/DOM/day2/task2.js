2ce9bb4e2a9b2bed4657dc6df4b7aeb4b7f2df9c
Nitishsingh001 <nitishsingh92057@gmail.com>
2024-11-01 13:19:53 +0530

    final submission

---
+++ js/DOM/day2/task2.js
@@ -0,0 +1,65 @@
+
+
+
+
+     // + _- */
+
+     // addition ṇ
+ 
+ 
+     if statement 
+
+
+
+     var num= 1009 ; 
+     
+     if (num == 1){
+       console.log("Monday"); 
+       
+     } else if(num == 2 ){
+       console.log("Tuesday"); 
+     } else if (num == 3 ){
+       console.log("wednesday");
+     } else if (num == 4){
+       console.log("thrusday")
+     } else {
+       console.log("Sunday"); 
+     }
+     
+     &&
+        
+               a and b     1 *1  =1    true  and ture = true 
+                           1*0 = 0   ture and false = false  
+                           0*1 =1    false and true = false 
+                           0*0 =0    false and false = false
+        
+       
+       var option = 1;
+     switch(option)
+     {
+     case 1 :
+     console.log("Day 1 : Scrum + Coding");
+     case 2 :
+     console.log("Day 2 : Scrum + Coding + Skillathon");
+     case 3 :
+     console.log("Day 3 : Scrum + Skillathon + Standups");
+     default :
+     console.log("Holiday");
+     }
+     
+     
+     var name = "udit"
+     
+      switch(name){
+        case "parag" :
+          console.log("This is parag"); 
+           break; 
+        case "diwakar" : 
+          console.log("This is diwakar"); 
+          break; 
+        case "Nitish" : 
+          console.log("This is Nitish"); 
+          break; 
+          default : 
+          console.log("This not our friend"); 
+      }
